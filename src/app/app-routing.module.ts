08a09cf7b3309d97c28b4b3fe7d803647a4bbfa0
Chris Schedler <schedler.chris@gmail.com>
2024-06-15 12:22:43 -0500

    Add blog route and page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [{ path: 'contact', component: ContactComponent }];
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'blog',
+  },
+  {
+    path: '**',
+    redirectTo: 'blog',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
