399487554587915b210b9cf124733c41bffa6f18
Chris Schedler <schedler.chris@gmail.com>
2024-06-15 12:26:11 -0500

    Add about page and routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
   {
     path: 'blog',
     loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule),
