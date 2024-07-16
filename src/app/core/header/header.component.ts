13eb915048a13760e0742a79a199483e8d0e63bc
Chris Schedler <schedler.chris@gmail.com>
2024-07-16 16:53:20 -0500

    Left logo centered links with theme button on right.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  themeSwitch() {
+    let htmlContainer = document.getElementById('oHTML');
+    let nowTheme = htmlContainer?.getAttribute('data-bs-theme');
+    if (nowTheme == 'dark') {
+      htmlContainer?.setAttribute('data-bs-theme', 'light');
+    } else {
+      htmlContainer?.setAttribute('data-bs-theme', 'dark');
+    }
+  }
 }
