b4f73748cf12b590a8260c68100469a8280709e8
Chris Schedler <schedler.chris@gmail.com>
2024-12-11 19:06:51 -0600

    Update big photo to remove fixed parallax effect.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
   contactSrc = '../../assets/contact.jpg';
   ngOnInit(): void {
-    let paraTop = document.getElementById('para-top');
+    let paraTop = document.getElementById('photo-top');
     if (paraTop) {
       paraTop.style.backgroundImage = 'url(' + this.contactSrc + ')';
     }
--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NotFoundComponent implements OnInit {
   picSrc = '../../assets/404.jpg';
   ngOnInit(): void {
-    let paraTop = document.getElementById('para-top');
+    let paraTop = document.getElementById('photo-top');
     if (paraTop) {
       paraTop.style.backgroundImage = 'url(' + this.picSrc + ')';
     }
